7546e9e32043a19105ccb54fb0b7b5a621da4078
evgzakharov <evgzakharov88@gmail.com>
2019-03-15 08:51:03 +0300

    add usage + fix small errors

--- src/main/kotlin/test/Main.kt
+++ src/main/kotlin/test/Main.kt
@@ -1,15 +1,31 @@
 package test
 
 import org.slf4j.LoggerFactory
+import java.io.File
 
 private val log = LoggerFactory.getLogger(::main.javaClass)
 
 fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        log.error("invalid input data (arguments is empty)")
+    if (args.size < 2) {
+        log.error("invalid input data")
+        log.info(usage())
+        return
     }
 
-    val users = args.asSequence()
+    when (args.first()) {
+        "-f" -> File(args[1]).useLines { lines ->
+            processData(lines)
+        }
+        "-d" -> {
+            val records = args.drop(1).asSequence()
+            processData(records)
+        }
+        else -> log.info(usage())
+    }
+}
+
+private fun processData(records: Sequence<String>) {
+    val users = records.asSequence()
         .map { it.toUserWithEmails() }
 
     log.info("merge users:")
@@ -17,3 +33,14 @@ fun main(args: Array<String>) {
         log.info(it.toString())
     }
 }
+
+private fun usage(): String = """
+    supported below usage:
+    -f <fileName>
+        there <fileName> is path to file with input data
+    -d '<dataLine1>' '<dataLine2>' ..
+        there <dataLineN> is a user data
+
+    Each line in file and dataLine1 must by in next format:
+    <userName> -> <email1>, <email2>, .. etc, there `userName` is user name and `emailN` is a list of user emails
+""".trimIndent()
